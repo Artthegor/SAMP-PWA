5bf3cbe4bdc3c52bf72a33aa3070d06bf6aff582
Artthegor <44845322+Artthegor@users.noreply.github.com>
2019-11-20 12:11:56 +0100

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', (e) => {
+  
+    var cacheName = 'scanettePWA-v1';
+var appShellFiles = [
+    '/index.html',
+    './manifest.json',
+    './style.css',
+    './produits.csv',
+    './js/app.js',
+    './js/DecoderWorjer.js',
+    './exif.js',
+    './js/job.js',
+    './images/logo.png',
+    './images/barcode-scanner.png',
+    './images/icon-cart.png',
+    './images/icon-setup.png',
+    './images/icon-transmit.png',
+    './favicon.ico',
+    './icons/icon-32.png',
+    './icons/icon-64.png',
+    './icons/icon-96.png',
+    './icons/icon-128.png',
+    './icons/icon-168.png',
+    './icons/icon-192.png',
+    './icons/icon-256.png',
+    './icons/icon-512.png',
+];
+    
+    
+    
+    
+    
+    
+});
